cedd735c0331b6c951da469380295a3f75b8af9e
Tobi Kehinde <ibidapoobe.tobi@gmail.com>
2022-03-22 17:48:52 +0100

    making it better

--- assets/script/chat.js
+++ assets/script/chat.js
@@ -1,4 +1,4 @@
-
+/* 
 $(document).ready(function () {
     $(".chat_on").click(function () {
         $(".Layout").toggle();
@@ -10,4 +10,69 @@ $(document).ready(function () {
         $(".chat_on").show();
     });
 
-});
+}); */
+
+class Chatbox {
+    constructor() {
+        this.args = {
+            openButton: document.querySelector('.chatbox__button'),
+            chatBox: document.querySelector('.chatbox__support'),
+            sendButton: document.querySelector('.send__button')
+        }
+
+        this.state = false;
+        this.messages = [];
+    }
+
+    display() {
+        const { openButton, chatBox, sendButton } = this.args;
+
+        openButton.addEventListener('click', () => this.toggleState(chatBox))
+
+        sendButton.addEventListener('click', () => this.onSendButton(chatBox))
+
+        const node = chatBox.querySelector('input');
+        node.addEventListener("keyup", ({ key }) => {
+            if (key === "Enter") {
+                this.onSendButton(chatBox)
+            }
+        })
+    }
+
+    toggleState(chatbox) {
+        this.state = !this.state;
+
+        if (this.state) {
+            chatbox.classList.add('chatbox--active')
+        } else {
+            chatbox.classList.remove('chatbox--active')
+        }
+    }
+
+    onSendButton(chatbox) {
+        var textField = chatbox.querySelector('input');
+        let text = textField.value
+        if (text === "") {
+            return;
+        }
+
+        let msg = { message: text }
+        this.messages.push(msg);
+        this.updateChatText(chatbox)
+        textField.value = ''
+
+    }
+
+    updateChatText(chatbox) {
+        var html = '';
+        this.messages.slice().reverse().forEach(function (item, index) {
+            html += '<div class="messages__item messages__item--operator">' + item.message + '</div>'
+        });
+
+        const chatmessage = chatbox.querySelector('.chatbox__messages');
+        chatmessage.innerHTML = html;
+    }
+}
+
+const chatbox = new Chatbox();
+chatbox.display();
